cf4f1a3e3ef29063ec20f4aa919f2f1d4e9796c5
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-10-07 11:02:22 +0700

    feat: change to unibuzz

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { Logo } from "./Logo"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl" backgroundColor={"#EBF2FE"}>
+    <Box textAlign="center" fontSize="xl" backgroundColor={"#F9F9F9"}>
       <Grid minH="100vh" p={3} >
         {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
        
@@ -28,9 +28,10 @@ export const App = () => (
       '350', // 48em-62em
       '400', // 62em+
     ]} h={"auto"}><Image src='title.png' alt='Dan Abramov' /></Box></Center>
-        <Heading as='h1' size='lg' pb={5} color={"#696969"} > Ethereum wallet tracker at your home screen!</Heading>
+        <Heading as='h1' size='lg' pb={5} color={"#2B2B2B"} > Anonymous Campus Community,</Heading>
+        <Heading as='h1' size='lg' pb={5} color={"#2B2B2B"} > Coming real soon to your campus!</Heading>
         <VStack >
-        <Link
+        {/* <Link
             color="transparent"
             href="https://apps.apple.com/id/app/widgeth-io/id1622378453"
             fontSize="2xl"
@@ -42,35 +43,39 @@ export const App = () => (
       '350', // 30em-48em
       '350', // 48em-62em
       '350', // 62em+
-    ]}><Image src='appstore.png' alt='Dan Abramov' /></Box></Center>
-          </Link>
+    ]}><Image src='appstore.png' alt='Unibuzz' /></Box></Center>
+          </Link> */}
         
 <Box > <Image maxW={[
       '250', // 0-30em
       '400', // 30em-48em
       '400', // 48em-62em
       '400', // 62em+
-    ]} src='simulator.png' alt='Dan Abramov' /></Box>
+    ]} src='simulator.png' alt='Unibuzz' /></Box>
        
           
           <HStack>
 
-          <Text>
+          <Text
+          color={"#2B2B2B"}
+          >
           Made with ❤️ by
           </Text>
           <Link
-            color="teal.500"
-            href="https://twitter.com/kevinahmad"
+            color={"#F2D96F"}
+            // href="https://twitter.com/kevinahmad"
             fontSize="2xl"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Kevin
+            Unibuzz
           </Link>
 
           </HStack>
-          <Text>
-          © 2022 Kevin Ahmad
+          <Text
+          color={"#2B2B2B"}
+          >
+          © 2022 Unibuzz.app
           </Text>
           
         </VStack>
