6ceb49a367e43e7d4729067dcd4214f9f2c02ac4
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-11-17 22:03:10 +0700

    ui in download

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -1,4 +1,18 @@
 import React, { useEffect } from "react";
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
+    Heading,
+    Image,
+    Center,
+    HStack
+  } from "@chakra-ui/react"
 
 function Tambah() {
 
@@ -7,9 +21,85 @@ function Tambah() {
   }, []);
 
   return (
-    <div>
-      <h2>Download Unibuzz</h2>
-    </div>
+    <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl" backgroundColor={"#F9F9F9"}>
+      <Grid minH="100vh" p={3} >
+       
+        <Center  p={10}><Box maxW={[
+      '150', // 0-30em
+      '350', // 30em-48em
+      '350', // 48em-62em
+      '400', // 62em+
+    ]} h={"auto"}><Image src='title.png' alt='Dan Abramov' /></Box></Center>
+        <Heading as='h1' size='lg' pb={5} color={"#2B2B2B"} > Anonymous Campus Community,</Heading>
+        <Heading as='h1' size='lg' pb={5} color={"#2B2B2B"} > Coming real soon to your campus!</Heading>
+        <VStack >
+
+        <Link
+            color="transparent"
+            href="https://apps.apple.com/id/app/unibuzz-campus-community/id6443903058"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Center  p={5}><Box maxW={[
+      '300', // 0-30em
+      '350', // 30em-48em
+      '350', // 48em-62em
+      '350', // 62em+
+    ]}><Image src='appstore.png' alt='Dan Abramov' /></Box></Center>
+          </Link>
+
+          <Link
+            color="transparent"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScAzilOtVOe2ES-XXvVihUfq3ynge5RwRegE0XoBAKCkiZdAQ/viewform"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Center  p={5}><Box maxW={[
+      '300', // 0-30em
+      '350', // 30em-48em
+      '400', // 48em-62em
+      '500', // 62em+
+    ]}><Image src='addMyUni.png' alt='Dan Abramov' /></Box></Center>
+          </Link>
+        
+<Box > <Image maxW={[
+      '250', // 0-30em
+      '400', // 30em-48em
+      '400', // 48em-62em
+      '400', // 62em+
+    ]} src='simulator.png' alt='Unibuzz' /></Box>
+       
+          
+          <HStack>
+
+          <Text
+          color={"#2B2B2B"}
+          >
+          Made with ❤️ by
+          </Text>
+          <Link
+            color={"#F2D96F"}
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Unibuzz
+          </Link>
+
+          </HStack>
+          <Text
+          color={"#2B2B2B"}
+          >
+          © 2022 Unibuzz.app
+          </Text>
+          
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
   );
 }
 
